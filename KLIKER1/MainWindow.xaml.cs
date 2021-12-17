c9b64f40ec71560b2579d4481b074d6512b3da3c
gr601_benan <gr601_benan@STUDVM-16>
2021-12-17 11:38:22 +0700

    Добавьте файлы проекта.

---
+++ KLIKER1/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KLIKER1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        long point = 110;
+        static int click = 1;
+        double sol_b1 = 10 + 10 * (30 + click * 0.2);
+        double sol_b2 = 20 + 20 * (60 + click * 0.4);
+        double sol_b3 = 30 + 30 * (90 + click * 0.6);
+        double sol_b4 = 40 + 40 * (120 + click * 0.8);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Update()
+        {
+            poi.Content = "Points:" + point;
+            cli.Content = "Points for click:" + click;
+            b1.Content = (sol_b1).ToString();
+            b2.Content = (sol_b2).ToString();
+            b3.Content = (sol_b3).ToString();
+            b4.Content = (sol_b4).ToString();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            point += click;
+            Update();
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            if(point >= (sol_b1))
+            {
+                point -= Convert.ToInt64(Math.Round(sol_b1));
+                click += 3;
+                Update();
+            }
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            if (point >= (sol_b2))
+            {
+                point -= Convert.ToInt64(Math.Round(sol_b2));
+                click += 4;
+                Update();
+            }
+        }
+
+        private void b3_Click(object sender, RoutedEventArgs e)
+        {
+            if (point >= (sol_b3))
+            {
+                point -= Convert.ToInt64(Math.Round(sol_b3));
+                click += 6;
+                Update();
+            }
+        }
+
+       
+        private void b4_Click(object sender, RoutedEventArgs e)
+        {
+            if (point >= (sol_b4))
+            {
+                point -= Convert.ToInt64(Math.Round(sol_b4));
+                click += 8;
+                Update();
+            }
+        }
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
+}
